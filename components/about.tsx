84365b8e0033ee734dfc68136d2599c2f806f0df
shamanth-k <shamanthk140@gmail.com>
2025-07-09 22:18:38 +0530

    edited resume

--- components/about.tsx
+++ components/about.tsx
@@ -56,7 +56,7 @@ const About = () => {
                   particularly interested in cloud technologies, modern
                   authentication systems, and building scalable applications.
                 </p>
-                <a href="/shamanth__resume.pdf" download>
+                <a href="/shamanth resume final.pdf" download>
                   <Button className="rounded-full">
                     <Download className="mr-2 h-4 w-4" /> Download Resume
                   </Button>
