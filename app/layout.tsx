9495f9648e6d94b1c0e77c824e72c3c4072a0773
shamanth-k <shamanthk140@gmail.com>
2025-07-23 12:18:29 +0530

    f

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,40 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import Navbar from "@/components/navbar"
-import Footer from "@/components/footer"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shamanth | Full Stack Developer",
-  description: "Portfolio of Shamanth, a Full Stack Developer & Tech Explorer",
-    generator: 'v0.dev'
-}
+  title: "shamanth",
+  description: "my portfolio website",
+  icons: {
+    icon: "/icon1.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
           <Navbar />
           {children}
           <Footer />
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
